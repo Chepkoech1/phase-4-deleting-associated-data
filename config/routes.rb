9301aa7bc24673d4a1c9b9a613860c1403e01b1b
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-18 21:42:25 +0300

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  resources :reviews, only: [:index]
-  resources :dog_houses, only: [:show]
+  # resources :reviews, only: [:index]
+  # resources :dog_houses, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dog_houses, only: [:show] do
+    # nested resource for reviews
+    resources :reviews, only: [:show, :index]
+    
+  end
+  # resources :reviews do
+  #   resources :comments
+  # end
+  resources :dog_houses do
+    resources :reviews do
+      resources :comments
+    end
+  end
+  # resources :reviews, only: [:show, :index, :create]
 end
